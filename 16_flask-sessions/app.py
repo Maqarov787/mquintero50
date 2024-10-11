75b927c98b5d12182feedf2cd63f6c485ba1bc7c
Marco Quintero <mquintero50@stuy.edu>
2024-10-11 10:33:52 -0400

    Push all work from yesterday

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -7,29 +7,39 @@ K15 - Flask Forms
 time spent: 0.5
 '''
 
+
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-from flask import session           #facilitate user session
-from flask import redirect          #facilitate user to different route
+from flask import session
+from flask import redirect
+import os
 
 app = Flask(__name__)    #create Flask object
-app.secret_key = 'BAD_SECRET_KEY' #randomly generate one later
+secret_hehe = os.urandom(32)
+app.secret_key = secret_hehe
 
 @app.route("/")
-def disp_loginpage():
-    return render_template( 'login.html' , method = request.method)
+def login_page():
+    if "username" in session:
+        return redirect("/response")
+    return render_template('login.html' , method = request.method)
 
+@app.route("/response", methods=['GET', 'POST']) # , methods=['GET', 'POST'])
+def response_page():
+    if request.method == "POST":
+        # at this point "username" must be in session
+        session["username"] = request.form["username"]
+    if "username" not in session:
+        return redirect("/")
+    return render_template('response.html', username=session["username"], method=request.method)
 
-@app.route("/response", methods=['POST']) # , methods=['GET', 'POST'])
-def authenticate():
-    session['username'] = request.form['username']
-    return render_template('response.html', username=session['username'], method=request.method)
-'''@
-def logout():'''
-    
+@app.route("/logout", methods = ['POST'])
+def logout_page():
+    # code to logout page and pop session
+    session.pop("username")
+    return redirect("/")
 
-    
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
