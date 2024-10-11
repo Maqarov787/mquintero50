df030f499c38ae9264ab5a665888ae3aa54ebe95
Marco Quintero <mquintero50@stuy.edu>
2024-10-11 10:59:44 -0400

    Added more comments based on code example

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -2,8 +2,8 @@
 Marco Quintero
 Ghidorah - Mark, Danny,Marco
 SoftDev
-K15 - Flask Forms
-2024-10-07
+K15 - Flask Sessions
+2024-10-11
 time spent: 0.5
 '''
 
@@ -22,7 +22,7 @@ app.secret_key = secret_hehe
 @app.route("/")
 def login_page():
     if "username" in session:
-        return redirect("/response")
+        return redirect("/response") #Could use url_for({method name as string}) instead for more complex URLs
     return render_template('login.html' , method = request.method)
 
 @app.route("/response", methods=['GET', 'POST']) # , methods=['GET', 'POST'])
@@ -37,10 +37,11 @@ def response_page():
 @app.route("/logout", methods = ['POST'])
 def logout_page():
     # code to logout page and pop session
-    session.pop("username")
+    session.pop("username", None) #Prevents KeyError from loading when logout is loaded separately
     return redirect("/")
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+    
