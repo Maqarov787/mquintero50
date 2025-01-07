9e8faa31dff486d52bd4a581288942c5ad3c2316
Marco Quintero <mquintero50@stuy.edu>
2025-01-06 22:24:11 -0500

    End of class

---
+++ 27_js/fibfac.js
@@ -0,0 +1,47 @@
+//Team Mexican Warriors :: Tahmim Hassan, Marco Quintero
+//SoftDev pd04
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+let fact(function n){
+  if (n == 1){
+    return n;
+  }
+  return n * fact(n-1);
+}
+//<your team's fact(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+fact(1)
+fact(2)
+fact(3)
+fact(4)
+
+//-----------------------------------------------------------------
+
+
+//fib:
+let fib(function n){
+  if (n == 0){
+    return n;
+  }
+  if (n == 0){
+    return n;
+  }
+  return fib(n-1) + fib(n-2);
+}
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+fib(0)
+fib(1)
+fib(2)
+fib(3)
+fib(4)
+
+//=================================================================
